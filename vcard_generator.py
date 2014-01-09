b8bbde89a823c14d75893eb31cfc32dffa8af6f5
davidak <git@davidak.de>
2014-01-09 05:34:03 +0100

    Kommandozeilen-Interface hinzugefügt, viele Verbesserungen am Code

--- vcard_generator.py
+++ vcard_generator.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
 """
-Der Python Random VCard-Generator generiert VCards mit zufälligen, aber plausiblen Daten.
+Der Random VCard-Generator generiert VCards mit zufälligen, aber plausiblen Daten.
 
-Copyright (C) 2013 davidak
+Copyright (C) 2013, 2014 davidak
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,48 +20,92 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see {http://www.gnu.org/licenses/}.
 """
 
+import argparse
 import random as r
 from datetime import datetime as date
 from pyzufall.person import Person
 
-__version__ = '0.4'
+__version__ = '0.5'
 
-gruppen = ['Arbeit', 'Freunde', 'Familie', 'Sportverein', 'Ärzte', 'Piratenpartei', 'CCC', 'Bekannte aus dem Internet', 'Kegelclub', 'Mafia', 'Expartner', 'Prostituierte']
+parser = argparse.ArgumentParser()
+parser.add_argument('-V', '--version', action='version', version='Random VCard-Generator ' + __version__)
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action ="store_true", help="increase output verbosity")
+group.add_argument("-q", "--quiet", action="store_true", help="no output")
+parser.add_argument("-c", "--count", type=int, default=1, help="number of vcards to generate")
+parser.add_argument("-o" ,"--output", default="Kontakte.vcf", help="output filename")
+args = parser.parse_args()
 
+gruppen = ['Arbeit', 'Kunden', 'Freunde', 'Familie', 'Sportverein', 'Ärzte', 'Piratenpartei', 'CCC', 'Bekannte aus dem Internet']
 
-p = Person()
+def generate_vcard():
+	_p = Person()
 
-print("BEGIN:VCARD")
-print("VERSION:3.0")
-print("N:" + p.nachname + ";" + p.vorname + ";;;")
-print("FN:" + p.name)
-#print("NICKNAME:Broho")
-#if r.randint(1,5) == 1:
-#	print("X-MAIDENNAME:" + z.nachname())
-bday = date.strptime(p.geburtsdatum, "%d.%m.%Y").date()
-print("BDAY;VALUE=DATE:" + date.strftime(bday, "%Y-%m-%d"))
-#print("ORG:" + z.firma() + ";Abteilung")
-if r.randint(1,4) == 1: # jeder 5. Kontakt von Arbeit
-	print("TITLE:" + p.beruf)
-print("CATEGORIES:" + r.choice(gruppen))
-#print("ADR;TYPE=WORK:;;Plorach Straße 27;Klostein;;46587;Deutschland")
-#print("EMAIL;TYPE=INTERNET:" + p.email)
-#print("URL;TYPE=WORK:" + p.webseite)
-note = ''
-if r.randint(0,1):
-	note += 'Interessen: ' + p.interessen
-if r.randint(0,1):
-	if note:
-		note += '\\n'
-	note += 'Lieblingsfarbe: ' + p.lieblingsfarbe
-if r.randint(0,1):
-	if note:
-		note += '\\n'
-	note += 'Lieblingsessen: ' + p.lieblingsessen
-if r.randint(0,1):
-	if note:
-		note += '\\n\\n'
-	note += 'Motto: ' + p.motto
-print("NOTE:" + note)
-print("END:VCARD")
-print('') #Zeilenumbruch
+	_s = "BEGIN:VCARD\n"
+	_s += "VERSION:3.0\n"
+	_s += "N:{};{};;;\n".format(_p.nachname, _p.vorname)
+	_s += "FN:{}\n".format(_p.name)
+	#_s+= "NICKNAME:Broho\n"
+	#if r.randint(1,5) == 1:
+		#_s += "X-MAIDENNAME:" + z.nachname() + "\n"
+	_bday = date.strptime(_p.geburtsdatum, "%d.%m.%Y").date()
+	_s += "BDAY;VALUE=DATE:{}\n".format(_bday)
+	#_s = "ORG:" + z.firma() + ";Abteilung\n"
+	if r.randint(1,4) == 1:
+		_s += "TITLE:{}\n".format(_p.beruf)
+	_s += "CATEGORIES:{}\n".format(r.choice(gruppen))
+	#_s += "ADR;TYPE=WORK:;;Plorach Straße 27;Klostein;;46587;Deutschland\n"
+	#_s += "EMAIL;TYPE=INTERNET:" + _p.email + "\n"
+	#_s += "URL;TYPE=WORK:" + _p.webseite + "\n"
+
+	# Notiz zusammensetzen
+	_note = ''
+	if r.randint(0,1):
+		_note += 'Interessen: ' + _p.interessen
+	if r.randint(0,1):
+		if _note:
+			_note += '\\n'
+		_note += 'Lieblingsfarbe: ' + _p.lieblingsfarbe
+	if r.randint(0,1):
+		if _note:
+			_note += '\\n'
+		_note += 'Lieblingsessen: ' + _p.lieblingsessen
+	if r.randint(0,1):
+		if _note:
+			_note += '\\n\\n'
+		_note += 'Motto: ' + _p.motto
+	if _note:
+		_s += "NOTE:{}\n".format(_note)
+
+	_s += "END:VCARD\n\n"
+	return _s
+
+def main():
+	if not args.quiet:
+		print("VCard-Generator {}\n".format(__version__))
+
+	output = ''
+
+	# Angegebene Anzahl an VCards generieren
+	for i in range(args.count):
+		output += generate_vcard()
+		# wenn es länger dauert, Fortschritt anzeigen
+		if args.count > 100:
+			if args.verbose:
+				print("VCard {} von {} generiert".format(i+1, args.count))
+			if not args.verbose and not args.quiet:
+				print('.', end='')
+
+	# Zeilenumbruch nach Punkten
+	if args.count > 100 and not args.verbose and not args.quiet:
+		print()
+
+	# VCards in Datei schreiben
+	with open(args.output, 'w') as f:
+		f.write(output)
+
+	if not args.quiet:
+		print("Fertig. {} VCard{} generiert und als {} gespeichert.".format(args.count, 's' if args.count > 1 else '', args.output))
+
+if __name__ == "__main__":
+	main()
